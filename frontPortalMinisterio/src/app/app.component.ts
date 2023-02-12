a5c9ef5799829d25b5722450d5eed7f0022d97a7
Lucasoviedo <lucasoviedolucas@gmail.com>
2023-02-12 00:04:25 -0300

    Cerrar sesion

--- frontPortalMinisterio/src/app/app.component.ts
+++ frontPortalMinisterio/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { ActivatedRoute, Router } from "@angular/router";
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontPortalMinisterio';
+
+  onlogin = true;
+
+  constructor(  private router: Router, 
+    private cookieService: CookieService,
+    private route: ActivatedRoute){
+      this.router.events.subscribe(event => {
+        if (this.router.url === `${environment.apiUrl}/login`) {
+          this.onlogin = true;
+        } else {
+          this.onlogin = false;
+        }
+      });
+    }
+
+
+
+      
+
+  
+
+  cerrarSesion(){
+    this.cookieService.delete('authToken');
+    this.router.navigate(['/login']);
+  }
 }
