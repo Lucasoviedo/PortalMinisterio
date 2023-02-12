ae515d5c3c6890d5f12b8ad8ec9bb025b035350a
Lucasoviedo <lucasoviedolucas@gmail.com>
2023-02-12 18:33:07 -0300

    Provincias List

--- frontPortalMinisterio/src/app/main/components/provincias/provincias.component.ts
+++ frontPortalMinisterio/src/app/main/components/provincias/provincias.component.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component , OnInit } from "@angular/core";
+import { CookieService } from 'ngx-cookie-service';
+import { IProvincia } from "src/app/core/models/i-provincia";
+import { ProvinciaService } from "../../api/resources/provincias.service";
 
 @Component({
     selector: 'app-provincias',
@@ -6,6 +9,18 @@ import { Component } from "@angular/core";
     styleUrls: ['./provincias.component.css','../generalStyles.css']
 })
 
-export class ProvinciasComponent{
+export class ProvinciasComponent implements OnInit {
 
+    provinciasData: Array<IProvincia> = [];
+
+    constructor(private provinciaService: ProvinciaService, 
+        private cookieService: CookieService) { }
+
+    ngOnInit(){
+        this.provinciaService.getProvincias(1)
+        .subscribe((response: any) => {
+            this.provinciasData = response
+            console.log(this.provinciasData);
+        });
+    }
 }
